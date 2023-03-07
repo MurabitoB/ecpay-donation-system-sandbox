4360c4dd9d3329be454b987a6e02b1b995c1d4f5
MurabitoB <enemarykawai@gmail.com>
2023-03-07 18:44:56 +0800

    Auto-generated commit [ci skip]

--- src/app/pages/sandbox/models/donation.ts
+++
@@ -1,9 +0,0 @@
-import { ToForm } from 'src/app/utils/form-utils';
-
-export interface Donation {
-  amount: number;
-  author: string;
-  message: string;
-  authorProfileImageUrl: string;
-  createTime: string;
-}
